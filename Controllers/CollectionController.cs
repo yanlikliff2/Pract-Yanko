db53d288389431e5edf801d1bec7239cf7ec4faf
yanlikliff2 <167241536+yanlikliff2@users.noreply.github.com>
2024-12-06 11:37:16 +0300

    Add files via upload

---
+++ Controllers/CollectionController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Yanko_web3_v2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CollectionController : ControllerBase
+    {
+    }
+}
